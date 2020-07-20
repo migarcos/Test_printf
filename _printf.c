3151e1a8901462cbd4bd318e96c2a7c7453a8f28
Miguel <migark@hotmail.com>
2020-07-20 21:55:45 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -5,14 +5,17 @@
  * _printf - 0x11 to project
  * @format: 
  *
- * return: 0 all is well, 1 to error on args or format
+ * return: 0 all is well, -1 to error on args or format
  * */
 
 int _printf(const char *format, ...)
 {
 	va_list valist;
-	
-	va_start(valist, WhatsHere);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(valist, format);
 
 	va_end(valist);
 
