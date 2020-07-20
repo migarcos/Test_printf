a3e14f0dbf751f504d6ee38a402912eb8feec76f
Miguel <migark@hotmail.com>
2020-07-20 21:48:09 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,20 @@
 #include "holberton.h"
+#include  <stdarg.h>
+
 /**
+ * _printf - 0x11 to project
+ * @format: 
  *
- *
- *
+ * return: 0 all is well, 1 to error on args or format
  * */
+
+int _printf(const char *format, ...)
+{
+	va_list valist;
+	
+	va_start(valist, WhatsHere);
+
+	va_end(valist);
+
+	return (0);
+}
