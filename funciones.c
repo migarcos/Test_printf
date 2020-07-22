8b3a086993fda0d073017cee4efae60c68527e7b
jhonalex1199 <2050@holbertonschool.com>
2020-07-22 04:57:04 +0000

    c

--- funciones.c
+++ funciones.c
@@ -28,25 +28,9 @@ int fstrings(va_list args)
  */
 int print_char(va_list args)
 {
-	char c;
+	int i = 0;
 
-	c = va_arg(args, int);
-	putchar(c);
+	i = va_arg(args, int);
+	putchar(i);
 	return (1);
-}
-
-/**
- * print_string - print a string
- * @args: list of args
- * Return: none
- */
-void print_string(va_list args)
-{
-	int i;
-	char *string = va_arg(args, char *);
-
-	for (i = 0; string[i] != '\0'; i++)
-	{
-		putchar(string[i]);
-	}
 }
